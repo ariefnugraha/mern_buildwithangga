77e6a6595bc25d2af0a3d6b7e9d8e3f7c54ff431
Arief <muhammadariefnugraha77@gmail.com>
2021-08-11 11:06:13 +0700

    add most pick, categories component

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,24 @@
 import React, {Component} from 'react'
+import landingPage from '../json/landingPage.json';
 
 import Header from '../parts/Header';
+import Hero from '../parts/Hero';
+import MostPick from '../parts/MostPick';
+import Categories from 'parts/Categories';
+
 export default class LandingPage extends Component {
+    constructor(props) {
+        super(props);
+        this.mostPicked = React.createRef();
+    }
+
     render() {
         return (
             <>
                 <Header {...this.props}></Header>
+                <Hero refMostPicked={this.mostPicked} data={landingPage.hero} />
+                <MostPick refMostPicked={this.mostPicked} data={landingPage.mostPicked} />
+                <Categories data={landingPage.categories} />
             </>
         )
     }
